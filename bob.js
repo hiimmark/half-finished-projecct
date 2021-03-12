fea14366c113e8c62763e23455ffba14e6ec8a5b
hiimmark <76633497+hiimmark@users.noreply.github.com>
2021-03-11 19:52:32 -0500

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,13 @@
+class Bob extends Baseclass{
+    constructor(x,y){
+        super(x,y,100,100)
+
+        
+    }
+    /*display(){
+        //this.body.position.x = this.x
+        //this.body.position.y = this.y
+        super.display();
+    }*/
+    
+}
